8cd93c519d3e636511822b26cf4c12b7ff404d50
louis_wang <louisw2015@gmail.com>
2017-03-23 12:17:33 -0400

    add comments

--- LinkedList/61_RotateList .py
+++ LinkedList/61_RotateList .py
@@ -1,3 +1,10 @@
+"""
+1. count # of nodes
+2. *connect the end node with the start node to make a circle
+3. *do k % length because k may be larger than length!
+4. break the circle right after the (k-1)th node
+"""
+
 class Solution(object):
     def rotateRight(self, head, k):
         """
